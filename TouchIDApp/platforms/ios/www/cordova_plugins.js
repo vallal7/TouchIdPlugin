9f560c4e5348bc07bd711debcee2b5bc3a504a4e
Ashwin G <gashwin@live.com>
2016-01-03 19:43:42 -0500

    Initial commit
    Initial commit

---
+++ TouchIDApp/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-touchid/www/touchid.js",
+        "id": "cordova-plugin-touchid.TouchID",
+        "pluginId": "cordova-plugin-touchid",
+        "clobbers": [
+            "touchid"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-touchid": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
